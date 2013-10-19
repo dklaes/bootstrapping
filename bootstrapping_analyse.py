d151d5081641606d146f76da5a46e063209ddb44
Dominik Klaes <dklaes@astro.uni-bonn.de>
2013-10-19 10:14:25 +0200

    "bootstrapping_analyse.py": More comments. Deleted importing chip offsets. Deleted multiprocessing. Deleted output with prefactors. "bootstrapping/results/results_prefactors.csv": File not longer needed at the moment.

--- bootstrapping_analyse.py
+++ bootstrapping_analyse.py
@@ -2,42 +2,48 @@
 # -*- coding: utf-8 -*-
 #scipy-0.11 and numpy-1.6.2 required!
 
+# Importing needed packages
 import numpy as np
 import sys
 import os
 import multiprocessing
 
+# Defining global variables
 global offset
 global NCHIPS
 global CHIPXMAX
 global CHIPYMAX
 
+# Reading command line arguments
 path = sys.argv[1] + "/bootstrapping/"
 NREL = int(sys.argv[2])
 NCHIPS = int(sys.argv[3])
 
+# Importing some global variables
 PIXXMAX = int((os.popen("echo ${CHIPGEOMETRY} | awk '{print $1}'").readlines())[0]) * int((os.popen("echo ${CHIPGEOMETRY} | awk '{print $3}'").readlines())[0])
 PIXYMAX = int((os.popen("echo ${CHIPGEOMETRY} | awk '{print $2}'").readlines())[0]) * int((os.popen("echo ${CHIPGEOMETRY} | awk '{print $4}'").readlines())[0])
 CHIPXMAX = float((os.popen("echo ${CHIPGEOMETRY} | awk '{print $3}'").readlines())[0])
 CHIPYMAX = float((os.popen("echo ${CHIPGEOMETRY} | awk '{print $4}'").readlines())[0])
 
+
+# Calculations for "true" solution
 fresiduals = open(path + "results/results_residuals.csv", "w")
-fprefactors = open(path + "results/results_prefactors.csv", "w")
 fcenters =  open(path + "results/results_centers.csv", "w")
 
-# Getting the prefactors.
+# Getting the prefactors of the true solution.
 A = float(((os.popen("cat " + path + "/chip_all.dat | grep A | awk '{print $3}'").readlines())[0]).strip())
 B = float(((os.popen("cat " + path + "/chip_all.dat | grep B | awk '{print $3}'").readlines())[0]).strip())
 C = float(((os.popen("cat " + path + "/chip_all.dat | grep C | awk '{print $3}'").readlines())[0]).strip())
 D = float(((os.popen("cat " + path + "/chip_all.dat | grep D | awk '{print $3}'").readlines())[0]).strip())
 E = float(((os.popen("cat " + path + "/chip_all.dat | grep E | awk '{print $3}'").readlines())[0]).strip())
 
+# Calculating the "true" center and write it into a file. The "true" solution is written as realisation 0.
 centerx = (-E/C-2.0*B/C*((2.0*A*E-C*D)/(C*C-4.0*A*B)))*PIXXMAX/2.0
 centery = ((2.0*A*E-C*D)/(C*C-4.0*A*B))*PIXYMAX/2.0
 fcenters.write("0 %.5f %.5f\n" %(centerx,centery))
 
 
-
+# Calculating statistics and center postions of each realisation.
 for i in range(NREL):
 	resbeforeALLCHIPS = np.array([])
 	resafterALLCHIPS = np.array([])
@@ -46,6 +52,7 @@ for i in range(NREL):
 		resbefore = np.fromfile(path + "/realisation_" + str(i+1) + "/chip_%i.csv" %(j+1), sep="\t").reshape((-1,15))[:,6]
 		resafter = np.fromfile(path + "/realisation_" + str(i+1) + "/chip_%i.csv" %(j+1), sep="\t").reshape((-1,15))[:,10]
 
+		# Calculating minimum, maximum and sigma of the residuals before and after fitting per chip.
 		sigmabefore = np.std(resbefore)
 		minimumbefore = np.amin(resbefore)
 		maximumbefore = np.amax(resbefore)
@@ -60,6 +67,7 @@ for i in range(NREL):
 
 		fresiduals.write("%i %i %f %f %f %f %f %f %f %f\n" %(i+1, j+1, sigmabefore, sigmaafter, minimumbefore, minimumafter, maximumbefore, maximumafter, meanbefore, meanafter))
 
+	# Calculating minimum, maximum and sigma of the residuals before and after fitting per realisation.
 	sigmabeforeALLCHIPS = np.std(resbeforeALLCHIPS)
 	minimumbeforeALLCHIPS = np.amin(resbeforeALLCHIPS)
 	maximumbeforeALLCHIPS = np.amax(resbeforeALLCHIPS)
@@ -71,41 +79,26 @@ for i in range(NREL):
 
 	fresiduals.write("%i 0 %f %f %f %f %f %f %f %f\n" %(i+1,sigmabeforeALLCHIPS,sigmaafterALLCHIPS,minimumbeforeALLCHIPS,minimumafterALLCHIPS,maximumbeforeALLCHIPS,maximumafterALLCHIPS,meanbeforeALLCHIPS,meanafterALLCHIPS))
 
-	PREFACTORSINPUT = np.loadtxt(path + "/realisation_" + str(i+1) + "/chip_all.dat", delimiter=" ", dtype={'names': ('factorname', 'equal', 'value', 'pm', 'error'), 'formats': ('S3', 'S1', 'S21', 'S2', 'S21')})
-
-	PREFACTORS = np.array([])
-	# 5 prefactors
-	for j in range(5):
-		PREFACTORS = np.append(PREFACTORS, (float(PREFACTORSINPUT[j][2]), float(PREFACTORSINPUT[j][4])))
-		fprefactors.write("%i %.21f %.21f\n" %(i+1,float(PREFACTORSINPUT[j][2]),float(PREFACTORSINPUT[j][4])))
-	PREFACTORS = PREFACTORS.reshape((-1,2))
-	A = PREFACTORS[0][0]
-	B = PREFACTORS[1][0]
-	C = PREFACTORS[2][0]
-	D = PREFACTORS[3][0]
-	E = PREFACTORS[4][0]
-
-  # Getting the prefactors.
-  A = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep A | awk '{print $3}'").readlines())[0]).strip())
-  B = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep B | awk '{print $3}'").readlines())[0]).strip())
-  C = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep C | awk '{print $3}'").readlines())[0]).strip())
-  D = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/"chip_all.dat | grep D | awk '{print $3}'").readlines())[0]).strip())
-  E = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep E | awk '{print $3}'").readlines())[0]).strip())
-
+	# Getting the prefactors and center position for one realisation.
+	A = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep A | awk '{print $3}'").readlines())[0]).strip())
+	B = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep B | awk '{print $3}'").readlines())[0]).strip())
+	C = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep C | awk '{print $3}'").readlines())[0]).strip())
+	D = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep D | awk '{print $3}'").readlines())[0]).strip())
+	E = float(((os.popen("cat " + path + "/realisation_" + str(i+1) + "/chip_all.dat | grep E | awk '{print $3}'").readlines())[0]).strip())
 
 	centerx = (-E/C-2.0*B/C*((2.0*A*E-C*D)/(C*C-4.0*A*B)))*PIXXMAX/2.0
 	centery = ((2.0*A*E-C*D)/(C*C-4.0*A*B))*PIXYMAX/2.0
 	fcenters.write("%i %.5f %.5f\n" %(i+1,centerx,centery))
 
 fresiduals.close()
-fprefactors.close()
 fcenters.close()
 
 centers = np.fromfile(path + "/results/results_centers.csv", sep="\t").reshape((-1,3))
 
-# Bootstrapping - true position
+# Calculating bootstrapping center positions - "true" center position
 cendiffs = centers[1:] - centers[0]
 
+# Calculating minimum, maximum, mean distance of all realisations to "true" center position for one run including sigma.
 meandiffx = np.mean(cendiffs[:,1])
 meandiffy = np.mean(cendiffs[:,2])
 mindiffx = np.amin(cendiffs[:,1])
@@ -127,9 +120,9 @@ fcenterprop.close()
 
 # Analysis of ellipse shape
 def calculatingeps(i):
-#  print("Start calculating data for chip " + str(i+1) + "/" + str(NCHIPS) + "...")
+  print("Calculating major and minor axis and (numerical) ellipticity for realisation " + str(i+1) + "...")
   
-  # Getting the prefactors.
+  # Getting the prefactors and calculate the center position.
   A = float(((os.popen("cat " + path + "chip_all.dat | grep A | awk '{print $3}'").readlines())[0]).strip())
   B = float(((os.popen("cat " + path + "chip_all.dat | grep B | awk '{print $3}'").readlines())[0]).strip())
   C = float(((os.popen("cat " + path + "chip_all.dat | grep C | awk '{print $3}'").readlines())[0]).strip())
@@ -145,12 +138,14 @@ def calculatingeps(i):
   # Creating arrays for xred (reduced and resized chip coordinates (for plotting and
   # numerical reasons)) and X (reduced (for plotting)). Having xred and X seperated doesn't
   # cause the problem of transforming the prefactors to the other coordinate system!
+  # As soon as possible not longer needed arrays are deleted from memory, otherwise too much
+  # memory is occupied.
 
   xred = 2.0*(np.arange(-8552, 0, 1))/PIXXMAX
   yred = 2.0*(np.arange(-8583, 0, 1))/PIXYMAX
 
   # Creating the corresponding meshgrids.
-  # Bottom left
+  # Bottom left part of the camera
   xxred, yyred = np.meshgrid(xred, yred)
   del xred
   del yred
@@ -189,7 +184,7 @@ def calculatingeps(i):
   area = np.append(area, len(epscond2.flatten()))
 
 
-  # bottom right
+  # Bottom right part of the camera.
   xred = 2.0*(np.arange(0, 8472, 1))/PIXXMAX
   yred = 2.0*(np.arange(-8583, 0, 1))/PIXYMAX
   # Creating the corresponding meshgrids.
@@ -231,7 +226,7 @@ def calculatingeps(i):
   area = np.append(area, len(epscond2.flatten()))
 
 
-  # Upper left
+  # Upper left part of the camera.
   xred = 2.0*(np.arange(-8552, 0, 1))/PIXXMAX
   yred = 2.0*(np.arange(0, 8430, 1))/PIXYMAX
   # Creating the corresponding meshgrids.
@@ -271,7 +266,7 @@ def calculatingeps(i):
   area = np.append(area, len(epscond2.flatten()))
 
 
-  # Upper right
+  # Upper right part of the camera.
   xred = 2.0*(np.arange(0, 8472, 1))/PIXXMAX
   yred = 2.0*(np.arange(0, 8430, 1))/PIXYMAX
   # Creating the corresponding meshgrids.
@@ -330,57 +325,6 @@ def calculatingeps(i):
   print("Numerical ellipticity: " + str(nume))
 
 
-# Reading chip offsets from config file
-offset=([])
-for k in range(NCHIPS):
-  a = int((os.popen("echo ${OFFSETX} | awk '{print $%i}'" %(k+1)).readlines())[0])
-  b = int((os.popen("echo ${OFFSETY} | awk '{print $%i}'" %(k+1)).readlines())[0])
-  offset = np.append(offset,[k,a,b])
-offset = offset.reshape((-1,3))
-
-#get the number of CPUs / cores
-totalcpus = multiprocessing.cpu_count()
-#maxcpus = int((os.popen("echo ${NPARA}").readlines())[0])
-maxcpus = 4
-usedcpus = maxcpus
-
-# Use only as many cores as chips are avaiable
-if NCHIPS < usedcpus:
-  usedcpus = NCHIPS
-
-
-print("Start calculating with " + str(usedcpus) + "/" + str(totalcpus) + " CPUs...")
-
-# Initialise process pool:
-pool = multiprocessing.Pool(usedcpus)
-
-# execute the calculating with a 'pool-map' command:
-catlist = []
-#for h in range(NCHIPS):
-for h in range(1):
-	catlist.append(h)
-
-#ALL = pool.map(calculatingeps, catlist)
-pool.map(calculatingeps, catlist)
-
-# Split the returning "ALL" array from "calculatingeps" into components:
-# - XXALL:		X meshgrid of reduced chip coordinates
-# - YYALL:		Y meshgrid of reduced chip coordinates
-# - epswoZPALL:	residuals without chip zeropoint offsets for reduced chip coordinates
-#maxdistance = np.array([])
-#area = np.array([])
-
-
-#for i in range(NCHIPS):
-#  maxdistance = np.append(maxdistance,ALL[i][0])
-#  area = np.append(area,ALL[i][1])
-
-# Area = Pi * a * b with a is major axis and b is minor axis. a is given by maxdistance so for b: b = Area / (Pi * a)
-#a = np.amax(maxdistance)
-#totalarea = np.sum(area)
-#totalarea = area
-#b = totalarea / (np.pi * a)
-#e = np.sqrt(a*a-b*b)
-
-#print (a,b,totalarea,e)
+for i in range(NREL):
+	calculatingeps(i)
 
