966b79c357d5e942b9a83c44fea8f3fbbfffd197
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-01-29 16:00:58 +0100

    "create_condor.sh": Modified for new paths.

--- create_condor.sh
+++ create_condor.sh
@@ -14,23 +14,23 @@ if [ ! -d "${MAIND}/bootstrapping" ]; then
   mkdir ${MAIND}/error
   mkdir ${MAIND}/output
 else
-#  rm -r ${MAIND}/bootstrapping
-#  rm -r ${MAIND}/log
-#  rm -r ${MAIND}/error
-#  rm -r ${MAIND}/output
-#  mkdir ${MAIND}/bootstrapping
-#  mkdir ${MAIND}/log
-#  mkdir ${MAIND}/error
-#  mkdir ${MAIND}/output
+  rm -r ${MAIND}/bootstrapping
+  rm -r ${MAIND}/log
+  rm -r ${MAIND}/error
+  rm -r ${MAIND}/output
+  mkdir ${MAIND}/bootstrapping
+  mkdir ${MAIND}/log
+  mkdir ${MAIND}/error
+  mkdir ${MAIND}/output
   echo ""
 fi
 
 echo "Universe   = vanilla" > submit
-echo "Executable = /vol/aibn41/aibn41_1/dklaes/bootstrapping/bootstrapping_new.sh" >> submit
-echo "transfer_input_files = /vol/aibn41/aibn41_1/dklaes/bootstrapping/ldac.py" >> submit
-echo "initialdir     = /vol/aibn41/aibn41_1/dklaes/bootstrapping" >> submit
-echo "remote_initialdir     = /vol/aibn41/aibn41_1/dklaes/bootstrapping" >> submit
-echo 'Requirements = (Memory > 2000)' >> submit
+echo "Executable = /vol/users/users/dklaes/git/bootstrapping/bootstrapping.sh" >> submit
+echo "transfer_input_files = /vol/users/users/dklaes/git/bootstrapping/ldac.py" >> submit
+echo "initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> submit
+echo "remote_initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> submit
+echo 'Requirements = (Memory > 1500)' >> submit
 echo "" >> submit
 
 i=${NRELSTART}
@@ -40,7 +40,7 @@ do
  echo "Log        = ${MAIND}/log/realisation_${i}.log" >> submit
  echo "Output     = ${MAIND}/output/realisation_${i}.out" >> submit
  echo "Error      = ${MAIND}/error/realisation_${i}.err" >> submit
- echo "Queue 1" >> submit
+ echo "Queue" >> submit
  echo "" >> submit
   
   i=$(( $i + 1 ))
