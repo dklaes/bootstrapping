0f363d2e80f2a6c0ce71565d4c755ebc56c52de6
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-01-31 15:58:22 +0100

    "create_condor.sh": Rewrote script such that now the submit file name can be given and the process and cluster number are included in the directory and filenames.

--- create_condor.sh
+++ create_condor.sh
@@ -1,47 +1,30 @@
 # Script for creating condor submission file
 
 # $1 = main directory where the files are
-# $2 = start number of realisations
-# $3 = end number of realisations
+# $2 = number of realisations
+# $3 = name of submit file
 
 MAIND=$1
-NRELSTART=$2
-NRELSTOP=$3
+NREL=$2
+OUTFILE=$3
 
 if [ ! -d "${MAIND}/bootstrapping" ]; then
   mkdir ${MAIND}/bootstrapping
   mkdir ${MAIND}/log
   mkdir ${MAIND}/error
   mkdir ${MAIND}/output
-else
-  rm -r ${MAIND}/bootstrapping
-  rm -r ${MAIND}/log
-  rm -r ${MAIND}/error
-  rm -r ${MAIND}/output
-  mkdir ${MAIND}/bootstrapping
-  mkdir ${MAIND}/log
-  mkdir ${MAIND}/error
-  mkdir ${MAIND}/output
-  echo ""
 fi
 
-echo "Universe   = vanilla" > ${MAIND}/submit
-echo "Executable = /vol/users/users/dklaes/git/bootstrapping/bootstrapping.sh" >> ${MAIND}/submit
-echo "transfer_input_files = /vol/users/users/dklaes/git/bootstrapping/ldac.py" >> ${MAIND}/submit
-echo "initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> ${MAIND}/submit
-echo "remote_initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> ${MAIND}/submit
-echo 'Requirements = (Memory > 1500)' >> ${MAIND}/submit
-echo "" >> ${MAIND}/submit
+echo "Universe   = vanilla" > ${MAIND}/${OUTFILE}
+echo "Executable = /vol/users/users/dklaes/git/bootstrapping/bootstrapping.sh" >> ${MAIND}/${OUTFILE}
+echo "transfer_input_files = /vol/users/users/dklaes/git/bootstrapping/ldac.py" >> ${MAIND}/${OUTFILE}
+echo "initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> ${MAIND}/${OUTFILE}
+echo "remote_initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> ${MAIND}/${OUTFILE}
+echo 'Requirements = (Memory > 1500)' >> ${MAIND}/${OUTFILE}
+echo "" >> ${MAIND}/${OUTFILE}
 
-i=${NRELSTART}
-while [ ${i} -le ${NRELSTOP} ]
-do
- echo "Arguments  = ${MAIND} ${i}" >> ${MAIND}/submit
- echo "Log        = ${MAIND}/log/realisation_${i}.log" >> ${MAIND}/submit
- echo "Output     = ${MAIND}/output/realisation_${i}.out" >> ${MAIND}/submit
- echo "Error      = ${MAIND}/error/realisation_${i}.err" >> ${MAIND}/submit
- echo "Queue" >> ${MAIND}/submit
- echo "" >> ${MAIND}/submit
-  
-  i=$(( $i + 1 ))
-done
+echo "Arguments  = ${MAIND} \$(Cluster).\$(Process)" >> ${MAIND}/${OUTFILE}
+echo "Log        = ${MAIND}/log/realisation_\$(Cluster).\$(Process).log" >> ${MAIND}/${OUTFILE}
+echo "Output     = ${MAIND}/output/realisation_\$(Clutser).\$(Process).out" >> ${MAIND}/${OUTFILE}
+echo "Error      = ${MAIND}/error/realisation_\$(Cluster).\$(Process).err" >> ${MAIND}/${OUTFILE}
+echo "Queue ${NREL}" >> ${MAIND}/${OUTFILE}
