0117b1e35f8830d8db39c2de2f86728845d0e1f7
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-01-31 18:32:40 +0100

    Undo serveral changes.

--- bootstrapping.sh
+++ bootstrapping.sh
@@ -15,25 +15,29 @@ FILTER=r
 
 # Creating the subfolders with data...
 mkdir ${MAIND}/bootstrapping/realisation_${NUMREL}
-
-${P_PYTHON} bootstrapping.py -i ${MAIND}/chip_all_filtered.cat -n ${NUMREL} -p ./ -t PSSC
+ls -l
+${P_PYTHON} bootstrapping.py -i ./chip_all_filtered.cat -n ${NUMREL} -p ./ -t PSSC
 
 # Fitting the data
-${P_PYTHON} illum_correction_fit.py -i ./chip_all_filtered.cat \
+${P_PYTHON} illum_correction_fit.py -i ./chip_all_filtered_bootstrapping.cat \
 				-t PSSC -p ./
 
 #Applying the fit-parameter to our catalog data...
-${P_PYTHON} illum_ldactools.py -i ./chip_all_filtered.cat \
-			-o ./chip_all_filtered_fitted.cat -t PSSC \
+${P_PYTHON} illum_ldactools.py -i ./chip_all_filtered_bootstrapping.cat \
+			-o ./chip_all_filtered_bootstrapping_fitted.cat -t PSSC \
 			-a CALCS_AFTER_FITTING \
 			-e "./coeffs.txt ${FILTER}"
 
-${P_PYTHON} illum_ldactools.py -i ./chip_all_filtered_fitted.cat -t PSSC \
+${P_PYTHON} illum_ldactools.py -i ./chip_all_filtered_bootstrapping_fitted.cat -t PSSC \
 			-a STATISTICS -e "./coeffs.txt 10 10" \
 			-o ./stats.txt
 
-${P_PYTHON} illum_ldactools.py -i ./chip_all_filtered_fitted.cat -t PSSC \
+${P_PYTHON} illum_ldactools.py -i ./chip_all_filtered_bootstrapping_fitted.cat -t PSSC \
 			-o ./fitting.txt -a MAG_DEPENDENCY
 
-cp *.txt *.cat ${MAIND}/bootstrapping/realisation_${NUMREL}/
-rm *.txt *.cat *.pyc
+cp stats.txt fitting.txt coeffs.txt ${MAIND}/bootstrapping/realisation_${NUMREL}/
+cp chip_all_filtered_bootstrapping.cat chip_all_filtered_bootstrapping_fitted.cat ${MAIND}/bootstrapping/realisation_${NUMREL}/
+
+rm stats.txt fitting.txt coeffs.txt
+rm chip_all_filtered.cat chip_all_filtered_bootstrapping.cat chip_all_filtered_bootstrapping_fitted.cat
+rm *.pyc
