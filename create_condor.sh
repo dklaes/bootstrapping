00923cdad1f70fecc78fa11dd33a228413256244
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-02-03 18:28:58 +0100

    "create_condor.sh": Went back to individual creation of queue command (there is no speed disadvantage) and creating individual initialdirs, meaning working directories.

--- create_condor.sh
+++ create_condor.sh
@@ -5,8 +5,11 @@
 # $3 = name of submit file
 
 MAIND=$1
-NREL=$2
-OUTFILE=$3
+NRELSTART=$2
+NRELSTOP=$3
+OUTFILE=$4
+
+
 
 if [ ! -d "${MAIND}/bootstrapping" ]; then
   mkdir ${MAIND}/bootstrapping
@@ -15,16 +18,29 @@ if [ ! -d "${MAIND}/bootstrapping" ]; then
   mkdir ${MAIND}/output
 fi
 
+i=${NRELSTART}
+while [ ${i} -le ${NRELSTOP} ]
+do
+  mkdir ${MAIND}/bootstrapping/realisation_${i}/
+  i=$(( $i + 1 ))
+done
+
+
 echo "Universe   = vanilla" > ${MAIND}/${OUTFILE}
 echo "Executable = /vol/users/users/dklaes/git/bootstrapping/bootstrapping.sh" >> ${MAIND}/${OUTFILE}
 echo "transfer_input_files = /vol/users/users/dklaes/git/bootstrapping/ldac.py" >> ${MAIND}/${OUTFILE}
-echo "initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> ${MAIND}/${OUTFILE}
-echo "remote_initialdir     = /vol/users/users/dklaes/git/bootstrapping" >> ${MAIND}/${OUTFILE}
 echo 'Requirements = (Memory > 1500)' >> ${MAIND}/${OUTFILE}
 echo "" >> ${MAIND}/${OUTFILE}
 
-echo "Arguments  = ${MAIND} \$(Cluster).\$(Process)" >> ${MAIND}/${OUTFILE}
-echo "Log        = ${MAIND}/log/realisation_\$(Cluster).\$(Process).log" >> ${MAIND}/${OUTFILE}
-echo "Output     = ${MAIND}/output/realisation_\$(Cluster).\$(Process).out" >> ${MAIND}/${OUTFILE}
-echo "Error      = ${MAIND}/error/realisation_\$(Cluster).\$(Process).err" >> ${MAIND}/${OUTFILE}
-echo "Queue ${NREL}" >> ${MAIND}/${OUTFILE}
+i=${NRELSTART}
+while [ ${i} -le ${NRELSTOP} ]
+do
+  echo "Arguments  = ${MAIND} ${i}" >> ${MAIND}/${OUTFILE}
+  echo "initialdir = ${MAIND}/bootstrapping/realisation_${i}/" >> ${MAIND}/${OUTFILE}
+  echo "Log        = ${MAIND}/log/realisation_${i}.log" >> ${MAIND}/${OUTFILE}
+  echo "Output     = ${MAIND}/output/realisation_${i}.out" >> ${MAIND}/${OUTFILE}
+  echo "Error      = ${MAIND}/error/realisation_${i}.err" >> ${MAIND}/${OUTFILE}
+  echo "Queue" >> ${MAIND}/${OUTFILE}
+  echo "" >> ${MAIND}/${OUTFILE}
+  i=$(( $i + 1 ))
+done
