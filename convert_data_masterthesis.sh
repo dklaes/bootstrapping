3413cfa711f9b7e7ff201f0d29e0696a5e0cecbb
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-03-12 15:41:27 +0100

    "convert_data_masterthesis.sh": Added filtering for older datasets.

--- convert_data_masterthesis.sh
+++ convert_data_masterthesis.sh
@@ -14,11 +14,47 @@ BOOT_SCRIPTS=/vol/users/users/dklaes/git/bootstrapping/
 FOLDER=${MD}/bootstrapping/realisation_${i}
 cp ${MD}/chip_all.csv ./chip_all_original.csv
 
-{
-mv ${FOLDER}/chip_all.dat ./coeffs.txt
+mv ${FOLDER}/chip_all.dat ./coeffs_old.txt
+grep A ./coeffs_old.txt > coeffs.txt
+grep B ./coeffs_old.txt >> coeffs.txt
+grep C ./coeffs_old.txt >> coeffs.txt
+grep D ./coeffs_old.txt >> coeffs.txt
+grep E ./coeffs_old.txt >> coeffs.txt
+grep F1 ./coeffs_old.txt >> coeffs.txt
+grep F2 ./coeffs_old.txt >> coeffs.txt
+grep F3 ./coeffs_old.txt >> coeffs.txt
+grep F4 ./coeffs_old.txt >> coeffs.txt
+grep F5 ./coeffs_old.txt >> coeffs.txt
+grep F6 ./coeffs_old.txt >> coeffs.txt
+grep F7 ./coeffs_old.txt >> coeffs.txt
+grep F8 ./coeffs_old.txt >> coeffs.txt
+grep F9 ./coeffs_old.txt >> coeffs.txt
+grep F10 ./coeffs_old.txt >> coeffs.txt
+grep F11 ./coeffs_old.txt >> coeffs.txt
+grep F12 ./coeffs_old.txt >> coeffs.txt
+grep F13 ./coeffs_old.txt >> coeffs.txt
+grep F14 ./coeffs_old.txt >> coeffs.txt
+grep F15 ./coeffs_old.txt >> coeffs.txt
+grep F16 ./coeffs_old.txt >> coeffs.txt
+grep F17 ./coeffs_old.txt >> coeffs.txt
+grep F18 ./coeffs_old.txt >> coeffs.txt
+grep F19 ./coeffs_old.txt >> coeffs.txt
+grep F20 ./coeffs_old.txt >> coeffs.txt
+grep F21 ./coeffs_old.txt >> coeffs.txt
+grep F22 ./coeffs_old.txt >> coeffs.txt
+grep F23 ./coeffs_old.txt >> coeffs.txt
+grep F24 ./coeffs_old.txt >> coeffs.txt
+grep F25 ./coeffs_old.txt >> coeffs.txt
+grep F26 ./coeffs_old.txt >> coeffs.txt
+grep F27 ./coeffs_old.txt >> coeffs.txt
+grep F28 ./coeffs_old.txt >> coeffs.txt
+grep F29 ./coeffs_old.txt >> coeffs.txt
+grep F30 ./coeffs_old.txt >> coeffs.txt
+grep F31 ./coeffs_old.txt >> coeffs.txt
+grep F32 ./coeffs_old.txt >> coeffs.txt
 NUM=`grep -c datapoints ./coeffs.txt`
 echo ${FOLDER} ${NUM} >> ${MD}/nums.txt
-} &
+
 
 mv ${FOLDER}/chip_all.csv ./
 
