494125720894312f89a910d2710db4fb4b47bdf8
Dominik Klaes <dklaes@astro.uni-bonn.de>
2014-04-08 16:25:41 +0200

    "create_condor.sh": - Deleted requirements. - Already creating log, error and output files in this step. This   increases the speed of condor_submit because this program creates   all these files and if this is done over a network, this is very   slow.

--- create_condor.sh
+++ create_condor.sh
@@ -22,6 +22,9 @@ i=${NRELSTART}
 while [ ${i} -le ${NRELSTOP} ]
 do
   mkdir ${MAIND}/bootstrapping/realisation_${i}/
+  touch ${MAIND}/log/realisation_${i}.log
+  touch ${MAIND}/output/realisation_${i}.out
+  touch ${MAIND}/error/realisation_${i}.err
   i=$(( $i + 1 ))
 done
 
@@ -29,7 +32,7 @@ done
 echo "Universe   = vanilla" > ${MAIND}/${OUTFILE}
 echo "Executable = /vol/users/users/dklaes/git/bootstrapping/bootstrapping.sh" >> ${MAIND}/${OUTFILE}
 echo "transfer_input_files = /vol/users/users/dklaes/git/bootstrapping/ldac.py" >> ${MAIND}/${OUTFILE}
-echo 'Requirements = (Memory > 550) && (Machine != "aibn79.astro.uni-bonn.de")' >> ${MAIND}/${OUTFILE}
+#echo 'Requirements = (Memory > 550) && (Machine != "aibn79.astro.uni-bonn.de")' >> ${MAIND}/${OUTFILE}
 echo "" >> ${MAIND}/${OUTFILE}
 
 i=${NRELSTART}
